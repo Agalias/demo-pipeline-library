d6e5cd4dee6f341eb9ef14f2664fbc34033727b9
Antons Gaļiaskarovs <antons.galiaskarovs@testdevlab.com>
2021-08-05 16:57:17 +0300

    k

--- vars/startSeleniumServer.groovy
+++ vars/startSeleniumServer.groovy
@@ -1,10 +1,8 @@
-def call(Map config) {
-    echo "${config.WEBDRIVER_PATH}"
-    echo "${config.time}"
+def call(String webdriverPath = WEBDRIVER_PATH, String time = TEST_TIME) {
     powershell """
-    Set-Location -Path '${config.webdriverPath}'
+    Set-Location -Path '${webdriverPath}'
     Start-Process -FilePath java -ArgumentList '-jar selenium-server-standalone-3.141.59.jar'
-    Start-Sleep -Seconds (120+'${config.time}')
+    Start-Sleep -Seconds (120+'${time}')
     Stop-Process -Force
     """
 }
