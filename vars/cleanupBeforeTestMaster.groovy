591c1b7896b96ae36133f5857a8ecd96f56abaf9
Antons Gaļiaskarovs <antons.galiaskarovs@testdevlab.com>
2021-08-12 11:17:35 +0300

    k

--- vars/cleanupBeforeTestMaster.groovy
+++ vars/cleanupBeforeTestMaster.groovy
@@ -1,10 +1,5 @@
 def call() {
     powershell '''
     Remove-Item C:/jenkins/workspace/TVM/* -Recurse -Force
-    taskkill /IM "TeamViewerMeeting.exe" /F
-    taskkill /IM "Zoom.exe" /F
-    Stop-Process -Id (Get-NetTCPConnection -LocalPort 4444).OwningProcess -Force
-    taskkill /IM "chrome.exe" /F
-    exit 0
     '''
 }
